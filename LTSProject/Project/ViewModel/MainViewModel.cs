77c58c59d935235331e83e6915e9eaebe6235d40
DGYJ-X <DGYJ-X@DESKTOP-YAOZJ>
2019-10-05 17:03:27 +0800

    升级Fluent.Ribbon到7.0

--- LTSProject/Project/ViewModel/MainViewModel.cs
+++ LTSProject/Project/ViewModel/MainViewModel.cs
@@ -1,34 +1,41 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Diagnostics;
+using System.Text;
 
 namespace Project.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            MessageCurrrent = "Open";
         }
+        private StringBuilder _message;
+        private string _messageCurrrent;
+
+        public StringBuilder Message { get => _message; set => Set(ref _message,value); }
+        public string MessageCurrrent { get => _messageCurrrent; set => Set(ref _messageCurrrent, value); }
+        public RelayCommand MainCommand1 { get => mainCommand1 ?? (mainCommand1 = new RelayCommand(Action1)); set => mainCommand1 = value; }
+        public RelayCommand MainCommand2 { get => mainCommand2 ?? (mainCommand2 = new RelayCommand(Action2)); set => mainCommand2 = value; }
+        public RelayCommand MainCommand3 { get => mainCommand3 ?? (mainCommand3 = new RelayCommand(Action3)); set => mainCommand3 = value; }
+        public RelayCommand MainCommand4 { get => mainCommand4 ?? (mainCommand4 = new RelayCommand(Action4)); set => mainCommand4 = value; }
+        public RelayCommand MainCommand5 { get => mainCommand5 ?? (mainCommand5 = new RelayCommand(Action5)); set => mainCommand5 = value; }
+        public RelayCommand MainCommand6 { get => mainCommand6 ?? (mainCommand6 = new RelayCommand(Action6)); set => mainCommand6 = value; }
+
+        private RelayCommand mainCommand1;
+        private RelayCommand mainCommand2;
+        private RelayCommand mainCommand3;
+        private RelayCommand mainCommand4;
+        private RelayCommand mainCommand5;
+        private RelayCommand mainCommand6;
+        private void Action1() 
+        {
+        }
+        private void Action2() { }
+        private void Action3() { }
+        private void Action4() { }
+        private void Action5() { }
+        private void Action6() { }
     }
 }
